73df430d0582f99c0a81d9785ba9cdafd2b600f0
Ben Newman <BenNewman@outlook.com>
2016-11-12 17:33:41 +0000

    Add parsing and uploading to Weather Underground

--- wsbridge.py
+++ wsbridge.py
@@ -1,8 +1,27 @@
+import sys
 import subprocess
 import shlex
+import json
+import urllib
+
+args = str(sys.argv)
 
 cmdtorun = shlex.split("rtl_433 -R 68 -F json -U -q")
 process = subprocess.Popen(cmdtorun, stdout=subprocess.PIPE)
 while True:
     output = process.stdout.readline()
-    
+    josn = json.loads(output)
+    wuformatted = {
+        "action": "updateraw",
+        "ID": args[1],
+        "PASSWORD": args[2],
+        "dateutc": josn['time'],
+        "windspeedmph": josn['speed'] / 1.609344,
+        "windgustmph": josn['gust'] / 1.609344,
+        "humidity": josn['humidity'],
+        "tempf": (josn['temperature_C'] * 1.8) + 32
+    }
+    requestdata = urllib.urlencode(wuformatted)
+    url = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?%s" % requestdata
+    result = urllib.urlopen(url)
+    print(result.read().decode('utf-8'))
