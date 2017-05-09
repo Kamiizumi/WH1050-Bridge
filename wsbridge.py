afe90638ca83b9014a0a37295840dcb0f270a6e7
Ben Newman <BenNewman@outlook.com>
2017-05-09 22:39:30 +0100

    Tidy / lint code (all vars are currently constants because they are global)

--- wsbridge.py
+++ wsbridge.py
@@ -1,3 +1,5 @@
+""" Script to parse rtl_433 output and send to Weather Underground """
+
 import sys
 import subprocess
 import shlex
@@ -5,25 +7,27 @@ import json
 import urllib
 from meteocalc import Temp, dew_point
 
-cmdtorun = shlex.split("rtl_433 -R 68 -F json -U -q")
-process = subprocess.Popen(cmdtorun, stdout=subprocess.PIPE)
+RTL_COMMAND = shlex.split("rtl_433 -R 68 -F json -U -q")
+RTL_PROCESS = subprocess.Popen(RTL_COMMAND, stdout=subprocess.PIPE)
 while True:
-    output = process.stdout.readline()
-    josn = json.loads(output)
-    reportedTemp = Temp(josn['temperature_C'], 'c')
-    calculatedDewPoint = dew_point(temperature=reportedTemp, humidity=josn['humidity'])
-    wuformatted = {
+    RTL_PROCESS_OUTPUT = RTL_PROCESS.stdout.readline()
+    OUTPUT_AS_JSON = json.loads(RTL_PROCESS_OUTPUT)
+    REPORTED_TEMP = Temp(OUTPUT_AS_JSON['temperature_C'], 'c')
+    CALCULATED_DEW_POINT = dew_point(temperature=REPORTED_TEMP, humidity=OUTPUT_AS_JSON['humidity'])
+    REQUEST_OBJECT = {
         "action": "updateraw",
         "ID": sys.argv[1],
         "PASSWORD": sys.argv[2],
-        "dateutc": josn['time'],
-        "windspeedmph": josn['speed'] / 1.609344,
-        "windgustmph": josn['gust'] / 1.609344,
-        "humidity": josn['humidity'],
-        "tempf": reportedTemp.f,
-        "dewptf": calculatedDewPoint.f
+        "dateutc": OUTPUT_AS_JSON['time'],
+        "windspeedmph": OUTPUT_AS_JSON['speed'] / 1.609344,
+        "windgustmph": OUTPUT_AS_JSON['gust'] / 1.609344,
+        "humidity": OUTPUT_AS_JSON['humidity'],
+        "tempf": REPORTED_TEMP.f,
+        "dewptf": CALCULATED_DEW_POINT.f
     }
-    requestdata = urllib.urlencode(wuformatted)
-    url = "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?%s" % requestdata
-    result = urllib.urlopen(url)
-    print(result.read().decode('utf-8'))
+    REQUEST_ENCODED = urllib.urlencode(REQUEST_OBJECT)
+    REQUEST_URL = \
+        "https://weatherstation.wunderground.com/weatherstation/updateweatherstation.php?%s" \
+        % REQUEST_ENCODED
+    REQUEST_RESULT = urllib.urlopen(REQUEST_URL)
+    print REQUEST_RESULT.read().decode('utf-8')
